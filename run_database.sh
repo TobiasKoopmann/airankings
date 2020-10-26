e489ddd42e88b878d617e2c3da6d097997b5cd77
Tobias Koopmann <tobias-koopmann@t-online.de>
2020-10-26 17:38:16 +0100

    kubernetes removal

--- run_database.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker build -t ls6-staff-registry.informatik.uni-wuerzburg.de/koopmann/airankings-database:latest -f kubernetes/Dockerfile .
-docker push ls6-staff-registry.informatik.uni-wuerzburg.de/koopmann/airankings-database:latest
-kubectl -n koopmann delete job airankings-database
-kubectl -n koopmann create -f kubernetes/airankings-database.yml
